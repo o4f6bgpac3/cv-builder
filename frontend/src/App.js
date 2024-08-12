6f2077601e32ac87b138031a2a86c6eef52f7d43
Mark Wade <mark.wade@makeit.com>
2024-08-12 15:25:22 +0100

    Add frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ const CVBuilder = () => {
     phone1: '',
     phone2: '',
     email: '',
-    statement: 'Use <br> to start a new line',
+    statement: '',
     skills: [
       '',
     ],
@@ -81,230 +81,26 @@ const CVBuilder = () => {
     });
   }, []);
 
-  const generateHTML = useCallback(() => {
-    return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>${cvData.name} - CV</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-        <style>
-          html {
-            margin: 1rem;
-          }
-          body {
-            font-family: Arial, sans-serif;
-          }
-          section {
-            margin-top: .75rem;
-            text-align: justify;
-            border-radius: 1.5rem;
-            background-color: #f8f8f8;
-            padding: 1rem;
-            justify-content: flex-start;
-            border: 2px solid gold;
-          }
-          #contact {
-            display: flex;
-            justify-content: flex-start;
-            background: #fff;
-            padding-bottom: 0;
-            border: none;
-          }
-          #contact #name {
-            display: flex;
-            line-height: 0.85;
-            margin: 0;
-            padding: 0;
-            font-size: 3rem;
-            font-weight: bold;
-          }
-          #contact #address {
-            margin-left: auto;
-            text-align: end;
-            justify-content: flex-start;
-            margin-top: 0;
-          }
-          .no-break {
-            page-break-inside: avoid;
-          }
-          h2 {
-            margin-top: 0;
-            margin-bottom: .25rem;
-          }
-          .interests > div,
-          #skills > div,
-          .indent {
-            padding: .25rem;
-          }
-          i {
-            width: 20px;
-            margin-right: .5rem;
-          }
-          .title {
-            text-decoration: underline;
-            margin-bottom: .5rem;
-          }
-          .indent {
-            margin-left: 1.5rem;
-          }
-          .ps-2 {
-            padding-top: .5rem;
-          }
-          .pe-0 {
-            padding-bottom: 0;
-          }
-          .mt-2 {
-            margin-top: .5rem;
-          }
-          .mb-1 {
-            margin-bottom: .25rem;
-          }
-          .fa-circle-check {
-            color: limegreen;
-          }
-          .fa-egg {
-            color: peachpuff;
-          }
-          .fa-seedling {
-            color: green;
-          }
-          @page {
-            size: A4;
-            margin: 0;
-          }
-          /* Ensure Key Skills and Professional Experience start on a new page if they don't fit */
-          #skills, .experience {
-            page-break-before: auto;
-          }
-          @media print {
-            .no-break {
-              page-break-inside: avoid;
-              padding-top: 20px;
-            }
-            
-            .no-break > *:first-child {
-              margin-top: 0;
-            }
-            
-            @page {
-              margin-top: 20px;
-            }
-            
-            .experience > div {
-              margin-bottom: 20px;
-            }
-            
-            body {
-              font-size: 12pt;
-            }
-            
-            h2 {
-              margin-top: 20px;
-            }
-          }
-
-          .experience > div {
-            padding: 15px;
-            border: 1px solid #e0e0e0;
-            border-radius: 5px;
-            margin-bottom: 15px;
-          }
-
-          .experience div > span {
-            font-weight: bold;
-            display: block;
-            margin-bottom: 10px;
-          }
-        </style>
-      </head>
-      <body>
-        <section id="contact">
-          <div id="name">${cvData.name}</div>
-          <div id="address">
-            <div>${cvData.address}</div>
-            <div>${cvData.phone1}</div>
-            <div>${cvData.phone2}</div>
-            <div>${cvData.email}</div>
-          </div>
-        </section>
-  
-        <section id="statement">
-          <h2>Personal Statement</h2>
-          <div>${cvData.statement}</div>
-        </section>
-  
-        <section id="skills">
-          <h2>Key Skills</h2>
-          <div>
-            ${cvData.skills.map(skill => `<div><i class="fa-regular fa-circle-check"></i>${skill}</div>`).join('')}
-          </div>
-        </section>
-  
-        <section class="experience">
-          <h2>Professional Experience</h2>
-          ${cvData.experience.map(exp => `
-            <div class="no-break">
-              <span>${exp.title}</span>
-              <div class="mt-2 mb-1">${exp.period}; ${exp.company}</div>
-              <ul>
-                ${exp.duties.map(duty => `<li>${duty}</li>`).join('')}
-              </ul>
-            </div>
-          `).join('')}
-        </section>
-  
-        <section>
-          <h2>Personal Interests</h2>
-          <div style="display: flex; justify-content: flex-start;">
-            <div class="interests">
-              ${cvData.interests.map(interest => `<div>${interest}</div>`).join('')}
-            </div>
-            <div></div>
-          </div>
-        </section>
-        <div style="display: flex; justify-content: flex-end;">
-          <small style="margin-top: .25rem;">Icons provided by FontAwesome</small>
-        </div>
-      </body>
-      </html>
-    `;
-  }, [cvData]);
-
   const generatePDF = useCallback(async () => {
-    const html = generateHTML();
     try {
-      const response = await fetch(`/api/generate-pdf`, {
+      const response = await fetch(`http://localhost:80/api/generate-pdf`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ html }),
+        body: JSON.stringify(cvData),
       });
 
       if (response.ok) {
-        // Check if the response is JSON (error) or PDF
-        const contentType = response.headers.get('content-type');
-        if (contentType && contentType.includes('application/json')) {
-          // It's an error message
-          const errorData = await response.json();
-          console.error('Error generating PDF:', errorData.error);
-          alert('Failed to generate PDF. Please try again.');
-        } else {
-          // It's a PDF
-          const blob = await response.blob();
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.style.display = 'none';
-          a.href = url;
-          a.download = 'cv.pdf';
-          document.body.appendChild(a);
-          a.click();
-          window.URL.revokeObjectURL(url);
-        }
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'cv.pdf';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
       } else {
         console.error('Failed to generate PDF:', response.statusText);
         alert('Failed to generate PDF. Please try again.');
@@ -313,7 +109,7 @@ const CVBuilder = () => {
       console.error('Error generating PDF:', error);
       alert('An error occurred while generating the PDF. Please try again.');
     }
-  }, [generateHTML]);
+  }, [cvData]);
 
   const downloadFile = (content, fileName, contentType) => {
     const blob = new Blob([content], { type: contentType });
@@ -332,7 +128,7 @@ const CVBuilder = () => {
     const baseName = cvData.name.replace(/\s+/g, '_');
   
     downloadFile(json, `${baseName}_CV_data.json`, 'application/json');
-  }, [cvData, generateHTML]);
+  }, [cvData]);
 
   const importJSON = useCallback((event) => {
     const file = event.target.files[0];
@@ -426,12 +222,12 @@ const CVBuilder = () => {
         <Paper elevation={3} sx={{ p: 3, mb: 3 }}>
           <Typography variant="h5" gutterBottom>Personal Statement</Typography>
           <TextField
-            fullWidth
-            multiline
-            rows={4}
-            label="Your personal statement"
-            value={cvData.statement}
-            onChange={(e) => handleChange('statement', null, null, e.target.value)}
+              fullWidth
+              multiline
+              rows={4}
+              label="Your personal statement"
+              value={cvData.statement}
+              onChange={(e) => handleChange('statement', null, null, e.target.value)}
           />
         </Paper>
 
